037b05385162b7d6e9d6526c1fc17c88fc4014f3
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-23 15:17:45 +0900

    Querying SwiftData objects

--- PracticeSwiftData/ContentView.swift
+++ PracticeSwiftData/ContentView.swift
@@ -5,17 +5,39 @@
 //  Created by 이안진 on 11/23/23.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(destinations) { destination in
+                    VStack(alignment: .leading) {
+                        Text(destination.name).font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .navigationTitle("iTour")
+            .toolbar {
+                Button("Add samples", action: addSamples)
+            }
         }
-        .padding()
+    }
+    
+    func addSamples() {
+        let rome = Destination(name: "Rome")
+        let florence = Destination(name: "Florence")
+        let naples = Destination(name: "Naples")
+        
+        modelContext.insert(rome)
+        modelContext.insert(florence)
+        modelContext.insert(naples)
     }
 }
 
