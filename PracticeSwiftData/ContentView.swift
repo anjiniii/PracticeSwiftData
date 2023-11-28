01d9940e58ff276f76198ed6293a2251f6c83431
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-28 13:14:45 +0900

    Sorting query results

--- PracticeSwiftData/ContentView.swift
+++ PracticeSwiftData/ContentView.swift
@@ -10,62 +10,38 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    
-    // 이름의 알파벳 순서
-    @Query(sort: \Destination.name) var destinations: [Destination]
-    
-    // 우선순위의 역순
-    // @Query(sort: \Destination.priority, order: .reverse) var destinations: [Destination]
-    
-    // 여러 정렬 조건이 필요한 경우
-    // @Query(sort: [SortDescriptor(\Destination.priority, order: .reverse), SortDescriptor(\Destination.name)]) var destinations: [Destination]
-    
     @State private var path = [Destination]()
+    @State private var sortOrder = SortDescriptor(\Destination.name)
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(destinations) { destination in
-                    NavigationLink(value: destination) {
-                        VStack(alignment: .leading) {
-                            Text(destination.name).font(.headline)
+            DestinationListingView(sort: sortOrder)
+                .navigationTitle("iTour")
+                .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
+                .toolbar {
+                    Button("Add Destination", systemImage: "plus", action: addDestination)
+                    
+                    Menu("Sort", systemImage: "arrow.up.arrow.down") {
+                        Picker("Sort", selection: $sortOrder) {
+                            Text("Name")
+                                .tag(SortDescriptor(\Destination.name))
                             
-                            Text(destination.date.formatted(date: .long, time: .shortened))
+                            Text("Priority")
+                                .tag(SortDescriptor(\Destination.priority))
+                            
+                            Text("Date")
+                                .tag(SortDescriptor(\Destination.date))
                         }
+                        .pickerStyle(.inline)
                     }
                 }
-                .onDelete(perform: deleteDestinations)
-            }
-            .navigationTitle("iTour")
-            .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
-            .toolbar {
-                Button("Add samples", action: addSamples)
-                Button("Add Destination", systemImage: "plus", action: addDestination)
-            }
         }
-    }
-    
-    func addSamples() {
-        let rome = Destination(name: "Rome")
-        let florence = Destination(name: "Florence")
-        let naples = Destination(name: "Naples")
-        
-        modelContext.insert(rome)
-        modelContext.insert(florence)
-        modelContext.insert(naples)
     }
     
     func addDestination() {
         let destination = Destination()
         modelContext.insert(destination)
         path = [destination]
-    }
-    
-    func deleteDestinations(_ indexSet: IndexSet) {
-        for index in indexSet {
-            let destination = destinations[index]
-            modelContext.delete(destination)
-        }
     }
 }
 
