5fbe88b303bdb3abdd23ed8d5ab38221ee51cd90
anjiniii <117328806+anjiniii@users.noreply.github.com>
2023-11-28 14:14:11 +0900

    Working with relationships

---
+++ PracticeSwiftData/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  PracticeSwiftData
+//
+//  Created by 이안진 on 11/28/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
